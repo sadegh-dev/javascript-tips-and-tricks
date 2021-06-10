a68345920833f35f9abc9e6be6e169d3b75f8eb4
charlie <sadegh.rez@gmail.com>
2021-06-10 15:45:08 +0430

    create accounts app

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# develop
+
+# Introduction new user-model
+#AUTH_USER_MODEL = 'accounts.User'
