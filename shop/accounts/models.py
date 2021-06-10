d991706868c4019bc03d2605a23ceac64da0f3d6
charlie <sadegh.rez@gmail.com>
2021-06-10 17:10:30 +0430

    create user-model

--- shop/accounts/models.py
+++ shop/accounts/models.py
@@ -3,9 +3,27 @@ from django.contrib.auth.models import AbstractBaseUser
 from .managers import MyUserManager
 
 class User(AbstractBaseUser):
-    email = models.EmailField(max_length=300, unique=True)
-    
+    email =     models.EmailField(max_length=300, unique=True)
+    full_name = models.CharField(max_length=300)
+    is_admin =  models.BooleanField(default=False)
+    is_active=  models.BooleanField(default=True)
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['full_name',]
+    objects = MyUserManager
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_lable):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
 
 
 
