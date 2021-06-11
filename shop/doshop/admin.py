abd874116ac2da125d835596a3249a63b2b5bec8
charlie <sadegh.rez@gmail.com>
2021-06-11 16:18:18 +0430

    action -admin

--- shop/doshop/admin.py
+++ shop/doshop/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
 from .models import Category, Product
 
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','slug')
+    prepopulated_fields = {'slug':('name',)}
 
-admin.site.register(Product)
 
 
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','price','available')
+    list_filter  = ('available','created')
+    list_editable = ('price',)
+    raw_id_fields = ('category',)
+    actions = ('make_avaliable',)
+
+    def make_avaliable(self, request, queryset) :
+        rows = queryset.update(available=True)
+        self.messages_user(request, f'{rows} updated')
+    
+    make_avaliable.short_description = 'make all available'
+    
 
